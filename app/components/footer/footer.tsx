c9f1cabb8403b181d0b02c367c0f99014f475562
MohammadShakaroun <aucemohamadchakaroun@gmail.com>
2023-09-06 16:21:03 +0300

    update footer data

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,28 +1,27 @@
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { AiFillFacebook} from "react-icons/ai";
 
 function Footer() {
   return (
     <motion.div 
     className="w-full"
-    whileTap={{ scale: 0.8 }}
-    style={{ x: 100 }}
     >
-      <div className="mx-auto px-4 bg-slate-600">
+      <div className="mx-auto px-4 bg-slate-600 p-10">
         <div className="flex flex-wrap text-left lg:text-left">
           <div className="w-full lg:w-6/12 px-4">
             <h4 className="text-3xl fonat-semibold text-blueGray-700">
               Let's keep in touch!
             </h4>
             <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
-              Find us on any of these platforms, we respond 1-2 business days.
+            10 Georgiou Gennadiou Street, Agathangelos Court, 3rd floor 3041 Limassol
             </h5>
             <div className="mt-6 lg:mb-0 mb-6">
               <button
                 className="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                 type="button"
               >
-                <i className="fab fa-twitter"></i>
+                <AiFillFacebook/>
               </button>
               <button
                 className="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
@@ -46,37 +45,37 @@ function Footer() {
           </div>
           <div className="w-full lg:w-6/12 px-4">
             <div className="flex flex-wrap items-top mb-6">
-              <div className="w-full lg:w-4/12 px-4 ml-auto">
+              <div className="w-full lg:w-5/12 px-4 ml-auto">
                 <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
                   Useful Links
                 </span>
                 <ul className="list-unstyled">
                   <li>
                     <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                      href="https://www.creative-tim.com/presentation?ref=njs-profile"
+                      className="text-blueGray-600 hover:text-blue-600 font-semibold block pb-2 text-sm"
+                      href="https://www.amathuslimassol.com/"
                     >
-                      About Us
+                      Amathus Hotels Rhodes
                     </a>
                   </li>
                   <li>
                     <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-                      href="https://www.creative-tim.com/bootstrap-themes/free?ref=njs-profile"
+                      className="text-blueGray-600 hover:text-blue-600 font-semibold block pb-2 text-sm"
+                      href="http://www.claridgepublic.com/"
                     >
-                      Free Products
+                      Claridge Public Ltd
                     </a>
                   </li>
                 </ul>
               </div>
-              <div className="w-full lg:w-4/12 px-4">
+              <div className="w-full lg:w-5/12 px-4">
                 <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
                   Other Resources
                 </span>
                 <ul className="list-unstyled">
                   <li>
                     <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                      className="text-blueGray-600 hover:text-blue-600 font-semibold block pb-2 text-sm"
                       href="https://github.com/creativetimofficial/notus-js/blob/main/LICENSE.md?ref=njs-profile"
                     >
                       MIT License
@@ -84,7 +83,7 @@ function Footer() {
                   </li>
                   <li>
                     <a
-                      className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
+                      className="text-blueGray-600 hover:text-blue-600 font-semibold block pb-2 text-sm"
                       href="https://creative-tim.com/contact-us?ref=njs-profile"
                     >
                       Contact Us
@@ -100,19 +99,13 @@ function Footer() {
           <div className="w-full md:w-4/12 px-4 mx-auto text-center">
             <div className="text-sm text-blueGray-500 font-semibold py-1">
               Copyright © <span id="get-current-year">2023</span>
-              <Link
-                href="https://www.creative-tim.com/product/notus-js"
-                className="text-blueGray-500 hover:text-gray-800"
-                target="_blank"
-              >
                 {" "}
-                Notus JS by
-              </Link>
+                Develop by {" "}
               <Link
-                href="https://www.creative-tim.com?ref=njs-profile"
-                className="text-blueGray-500 hover:text-blueGray-800"
+                href="https://www.aetelco.com"
+                className="text-blueGray-500 hover:text-blue-600"
               >
-                Creative Tim
+                AETELCO
               </Link>
               .
             </div>
